b20db62872c7cb4a8ad4dad1b0cf4b5829387936
abohalema98 <eng.mohameda7med@gmail.com>
2025-09-09 04:24:09 +0300

    Revert "no"
    This reverts commit 9406a5e4dbdcc96859203c4bb1fe761208d73d0b.

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
                 System.out.println("no active");
             }
 
-            String name = "My Name Is Mohamed Ahmed  3";
+            String name = "My Name Is Mohamed Ahmed 2";
 
             System.out.print(name);
 
