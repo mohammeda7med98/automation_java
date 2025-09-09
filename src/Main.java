a24649e71e66b2f93e1726072ef749595d36d8bd
abohalema98 <eng.mohameda7med@gmail.com>
2025-09-09 04:13:13 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+
+            int automation;
+            automation = 500;
+            int year = 2025;
+            double floating = 12.50;
+            char ne = 'a';
+            boolean isActive = false;
+//           double its means floating number.
+
+            if(isActive) {
+                System.out.println("active");
+            } else {
+                System.out.println("no active");
+            }
+
+            String name = "My Name Is Mohamed Ahmed";
+
+            System.out.print(name);
+
+    }
+}
+
