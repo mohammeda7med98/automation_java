2a2cfcd19654d767c0e5c39be4664f478b4f91f3
abohalema98 <eng.mohameda7med@gmail.com>
2025-09-10 22:17:39 +0300

    new lesson

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-            int automation;
-            automation = 500;
-            int year = 2025;
-            double floating = 12.50;
-            char ne = 'a';
-            boolean isActive = false;
-//           double its means floating number.
+//            int automation;
+//            automation = 500;
+//            int year = 2025;
+//            double floating = 12.50;
+//            char ne = 'a';
+//            boolean isActive = false;
+//         double its means floating number.
+//            if(isActive) {
+//                System.out.println("active");
+//            } else {
+//                System.out.println("no active");
+//            }
+//
+//            String name = "My Name Is Mohamed Ahmed";
+//
+//            System.out.print(name);
 
-            if(isActive) {
-                System.out.println("active");
-            } else {
-                System.out.println("no active");
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your name:");
+        String name = scanner.nextLine();
 
-            String name = "My Name Is Mohamed Ahmed";
+        System.out.println("And your age ?");
+        int age = scanner.nextInt();
 
-            System.out.print(name);
+        System.out.print("are you students? ");
+        boolean gender = scanner.nextBoolean();
+
+
+        System.out.println("hello " + name );
+        System.out.println("Your Age Is " + age + " years Old");
+        System.out.println("IS STD ? " + gender);
+
+        scanner.close();
 
     }
 }
